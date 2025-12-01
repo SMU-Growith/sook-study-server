487fa945f442ca51e22d55fda1a54bf22d89eda9
yoojinche <chesunny917@gmail.com>
2025-12-01 19:42:52 +0900

    ✨ feat: User 메소드 어노테이션 생성

---
+++ src/main/java/org/growith/be/growith/global/annotation/AuthenticatedMember.java
@@ -0,0 +1,13 @@
+package org.growith.be.growith.global.annotation;
+
+
+import io.swagger.v3.oas.annotations.Parameter;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target({ElementType.PARAMETER})
+@Parameter(hidden = true)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthenticatedMember {
+}
---
+++ src/main/java/org/growith/be/growith/global/annotation/AuthenticatedMemberResolver.java
@@ -0,0 +1,41 @@
+package org.growith.be.growith.global.annotation;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.growith.be.growith.domain.user.entity.User;
+import org.growith.be.growith.global.security.domain.CustomUserDetails;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticatedMemberResolver implements HandlerMethodArgumentResolver {
+
+    private final SecurityContextRepository securityContextRepository;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthenticatedMember.class)
+                && parameter.getParameterType().equals(User.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        try {
+            UserDetails userDetails = (UserDetails) securityContextRepository.loadDeferredContext(webRequest.getNativeRequest(HttpServletRequest.class)).get().getAuthentication().getPrincipal();
+            if (userDetails instanceof CustomUserDetails customUserDetails) {
+                return customUserDetails.getUser();
+            }
+            return null;
+        } catch (Exception e){
+            return null;
+        }
+    }
+}
