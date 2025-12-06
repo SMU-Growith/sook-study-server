b9212928c50fd904c83f6099965c08bb9be680f3
seoyeon <stellastar0921@gmail.com>
2025-12-07 03:41:03 +0900

    🐛 fix: 규칙카테고리, 스터디성향 enum 수정

--- src/main/java/org/growith/be/growith/domain/study/entity/enums/RuleCategory.java
+++ src/main/java/org/growith/be/growith/domain/study/entity/enums/RuleCategory.java
@@ -1,5 +1,9 @@
 package org.growith.be.growith.domain.study.entity.enums;
 
 public enum RuleCategory {
-    GENERAL, STUDY, NOTICE
+    TIME,       // 시간
+    FINE,       // 벌금
+    DAY_OFF,    // 휴무
+    ATMOSPHERE, // 분위기
+    ETC         // 기타
 }
--- src/main/java/org/growith/be/growith/domain/study/entity/enums/StudyStyleCategory.java
+++ src/main/java/org/growith/be/growith/domain/study/entity/enums/StudyStyleCategory.java
@@ -1,5 +1,8 @@
 package org.growith.be.growith.domain.study.entity.enums;
 
 public enum StudyStyleCategory {
-    STRICT, LOOSE
+    SYSTEMATIC, // 체계적인
+    FREE,       // 자유로운
+    COOPERATIVE,// 협력적인
+    RESULT_ORIENTED // 실적중심
 }
