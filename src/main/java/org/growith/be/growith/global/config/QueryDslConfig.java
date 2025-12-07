5d3f5f7936e7909b2cca746bc02a5fd2dbd435f3
yoojinche <chesunny917@gmail.com>
2025-12-07 22:09:22 +0900

    🐛 fix: QueryDSL 설정 파일 수정

--- src/main/java/org/growith/be/growith/global/config/QueryDslConfig.java
+++ src/main/java/org/growith/be/growith/global/config/QueryDslConfig.java
@@ -2,17 +2,18 @@ package org.growith.be.growith.global.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class QueryDslConfig {
+    @PersistenceContext
     EntityManager em;
 
     @Bean
-    JPAQueryFactory jpaQueryFactory(EntityManager em){
+    JPAQueryFactory jpaQueryFactory(){
         return new JPAQueryFactory(em);
     }
 }
