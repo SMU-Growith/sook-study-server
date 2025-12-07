36493432195942a38bd1ba306120d709437dc1f6
yoojinche <chesunny917@gmail.com>
2025-12-07 12:34:45 +0900

    🐛 fix: AuthenticatedUser 어노테이션 변경

--- src/main/java/org/growith/be/growith/domain/user/service/query/UserQueryServiceImpl.java
+++ src/main/java/org/growith/be/growith/domain/user/service/query/UserQueryServiceImpl.java
@@ -3,6 +3,8 @@ package org.growith.be.growith.domain.user.service.query;
 import lombok.RequiredArgsConstructor;
 import org.growith.be.growith.domain.user.entity.User;
 import org.growith.be.growith.domain.user.repository.UserRepository;
+import org.growith.be.growith.global.error.code.status.UserErrorCode;
+import org.growith.be.growith.global.error.exception.handler.UserException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +18,9 @@ public class UserQueryServiceImpl implements UserQueryService {
         return userRepository.findById(id).orElseThrow();
     }
 
-    public User getUserProfile(){
-        return null;
+    public User getUserProfile(Long userId){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserException(UserErrorCode.NOT_FOUND));
+        return user;
     }
 }
