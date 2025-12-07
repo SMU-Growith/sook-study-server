634376e8c2dfc182247accc321778f5da556861d
yoojinche <chesunny917@gmail.com>
2025-12-07 22:09:30 +0900

    🐛 fix: enum 값 추가

--- src/main/java/org/growith/be/growith/domain/user/entity/enums/Major.java
+++ src/main/java/org/growith/be/growith/domain/user/entity/enums/Major.java
@@ -6,8 +6,74 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Major {
-    COMPUTER("컴퓨터"),
-    DESIGN("디자인");
+    COMPUTER,
+    DESIGN,
+    KOREAN_LANGUAGE_AND_LITERATURE, // 한국어문학부
+    HISTORY_AND_CULTURE, // 역사문화학과
+    FRENCH_LANGUAGE_AND_CULTURE, // 프랑스언어·문화학과
+    CHINESE_LANGUAGE_AND_LITERATURE, // 중어중문학부
+    GERMAN_LANGUAGE_AND_CULTURE, // 독일언어·문화학과
+    JAPANESE_STUDIES, // 일본학과
+    LIBRARY_AND_INFORMATION_SCIENCE, // 문헌정보학과
+    CULTURAL_TOURISM_STUDIES, // 문화관광외식학부 문화관광학전공
+    LE_CORDON_BLEU_HOSPITALITY_MANAGEMENT, // 문화관광외식학부 르꼬르동블루외식경영전공
+    EDUCATION, // 교육학부
 
-    private final String description;
+    CHEMISTRY, // 화학과
+    LIFE_SCIENCE, // 생명시스템학부
+    MATHEMATICS, // 수학과
+    STATISTICS, // 통계학과
+    PHYSICAL_EDUCATION, // 체육교육과
+    DANCE, // 무용과
+
+    CHEMICAL_AND_BIOMOLECULAR_ENGINEERING, // 화공생명공학부
+    ARTIFICIAL_INTELLIGENCE_ENGINEERING, // 인공지능공학부
+    INTELLIGENT_ELECTRONIC_SYSTEMS, // 첨단소재·전자융합공학부 지능형전자시스템전공
+    ADVANCED_MATERIALS_PHYSICS, // 첨단소재·전자융합공학부 신소재물리전공
+    COMPUTER_SCIENCE, // 소프트웨어학부 컴퓨터과학전공
+    DATA_SCIENCE, // 소프트웨어학부 데이터사이언스전공
+    MECHANICAL_SYSTEMS_ENGINEERING, // 기계시스템학부
+    BASIC_ENGINEERING, // 기초공학부
+    ADVANCED_ENGINEERING, // 첨단공학부
+
+    FAMILY_RESOURCE_MANAGEMENT, // 가족자원경영학과
+    CHILD_WELFARE, // 아동복지학부
+    CLOTHING_AND_TEXTILES, // 의류학과
+    FOOD_AND_NUTRITION, // 식품영양학과
+
+    POLITICAL_SCIENCE_AND_INTERNATIONAL_RELATIONS, // 정치외교학과
+    PUBLIC_ADMINISTRATION, // 행정학과
+    ADVERTISING_AND_PUBLIC_RELATIONS, // 홍보광고학과
+    CONSUMER_ECONOMICS, // 소비자경제학과
+    SOCIAL_PSYCHOLOGY, // 사회심리학과
+
+    LAW, // 법학부
+    ECONOMICS, // 경제학부
+    BUSINESS_ADMINISTRATION, // 경영학부
+
+    PIANO, // 피아노과
+    ORCHESTRAL_INSTRUMENTS, // 관현악과
+    VOCAL_MUSIC, // 성악과
+    COMPOSITION, // 작곡과
+
+    PHARMACY, // 약학부
+    VISUAL_COMMUNICATION_DESIGN, // 시각·영상디자인과
+    INDUSTRIAL_DESIGN, // 산업디자인과
+    ENVIRONMENTAL_DESIGN, // 환경디자인과
+    CRAFTS, // 공예과
+    PAINTING, // 회화과
+
+    LIBERAL_ARTS, // 자유전공학부
+    BASIC_LIBERAL_ARTS, // 기초교양학부
+    INTERDISCIPLINARY_STUDIES, // 융합학부/연계전공
+
+    GLOBAL_CONVERGENCE_STUDIES, // 글로벌융합학부
+    GLOBAL_COOPERATION, // 글로벌협력전공
+    ENTREPRENEURSHIP, // 앙트러프러너십전공
+
+    ENGLISH_LANGUAGE_AND_LITERATURE, // 영어영문학전공
+    TESL, // 테슬(TESL)전공
+
+    MEDIA_STUDIES, // 미디어학부
+    ;
 }
