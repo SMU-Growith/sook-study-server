c0a9139c6a4cf26866bade4430c863f6eba02054
seoyeon <stellastar0921@gmail.com>
2025-12-12 21:32:48 +0900

    [FIX] 스터디 세션조회 캐싱 -> DB에서 직접 조회로 수정

--- src/main/java/org/growith/be/growith/domain/study/repository/StudySessionRepository.java
+++ src/main/java/org/growith/be/growith/domain/study/repository/StudySessionRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface StudySessionRepository extends JpaRepository<StudySession, Long> {
     @Query("SELECT COUNT(us) FROM UserStudy us " +
             "JOIN StudySession ss ON ss.study.id = us.study.id " +
@@ -14,4 +16,7 @@ public interface StudySessionRepository extends JpaRepository<StudySession, Long
 
     @Query("SELECT COUNT(ss) FROM StudySession ss WHERE ss.study.id = :studyId")
     Long countByStudyId(Long studyId);
+
+    // 스터디 ID로 세션 목록 조회 (회차 순으로 정렬)
+    List<StudySession> findByStudyIdOrderByNumberAsc(Long studyId);
 }
